4b4d5a5292260bef843c25d738cc001cfc45b7ab
Aleksey Pleshkov <im@alekseypleshkov.ru>
2017-11-27 18:23:39 +0300

    Update example and change build dir

--- example/config/index.js
+++ example/config/index.js
@@ -50,10 +50,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../example-dist/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../example-dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
